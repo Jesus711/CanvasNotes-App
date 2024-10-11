733a5ca77aa5de5e4b2f77ad3cd34b499279a98b
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2024-10-11 12:41:52 -0700

    Added a tooltip to each color. Modified the color list to be sorted in similarity: red, orange, yellow, ...etc

--- lib/pages/canvas_view.dart
+++ lib/pages/canvas_view.dart
@@ -20,36 +20,47 @@ class _CanvasViewState extends State<CanvasView> {
   final _nameController = TextEditingController();
 
   static List colorsList = [
+    // Reds
     [0, "Red", Colors.red.shade600],
-    [1, "Green", Colors.green.shade600],
-    [2, "Blue", Colors.blue.shade600],
-    [3, "Yellow", Colors.yellow.shade600],
+    [1, "Red Accent", Colors.redAccent.shade400],
+    [2, "Pink", Colors.pink.shade600],
+    [3, "Pink Accent", Colors.pinkAccent.shade400],
+
+    // Oranges and Yellows
     [4, "Orange", Colors.orange.shade600],
-    [5, "Purple", Colors.purple.shade600],
-    [6, "Pink", Colors.pink.shade600],
-    [7, "Brown", Colors.brown.shade600],
-    [8, "Grey", Colors.grey.shade600],
-    [9, "Cyan", Colors.cyan.shade600],
-    [10, "Lime", Colors.lime.shade600],
-    [11, "Teal", Colors.teal.shade600],
-    [12, "Indigo", Colors.indigo.shade600],
-    [13, "Amber", Colors.amber.shade600],
-    [14, "Deep Orange", Colors.deepOrange.shade600],
-    [15, "Deep Purple", Colors.deepPurple.shade600],
+    [5, "Deep Orange", Colors.deepOrange.shade600],
+    [6, "Orange Accent", Colors.orangeAccent.shade400],
+    [7, "Amber", Colors.amber.shade600],
+    [8, "Yellow", Colors.yellow.shade600],
+    [9, "Yellow Accent", Colors.yellowAccent.shade400],
+
+    // Greens
+    [10, "Green", Colors.green.shade600],
+    [11, "Light Green", Colors.lightGreen.shade600],
+    [12, "Green Accent", Colors.greenAccent.shade400],
+    [13, "Teal", Colors.teal.shade600],
+    [14, "Teal Accent", Colors.tealAccent.shade400],
+
+    // Blues
+    [15, "Blue", Colors.blue.shade600],
     [16, "Light Blue", Colors.lightBlue.shade600],
-    [17, "Light Green", Colors.lightGreen.shade600],
-    [18, "Blue Grey", Colors.blueGrey.shade600],
-    [19, "Pink Accent", Colors.pinkAccent.shade400],
-    [20, "Red Accent", Colors.redAccent.shade400],
-    [21, "Purple Accent", Colors.purpleAccent.shade400],
-    [22, "Indigo Accent", Colors.indigoAccent.shade400],
-    [23, "Cyan Accent", Colors.cyanAccent.shade400],
-    [24, "Orange Accent", Colors.orangeAccent.shade400],
-    [25, "Lime Accent", Colors.limeAccent.shade400],
-    [26, "Yellow Accent", Colors.yellowAccent.shade400],
-    [27, "Green Accent", Colors.greenAccent.shade400],
-    [28, "Teal Accent", Colors.tealAccent.shade400],
-    [29, "Blue Accent", Colors.blueAccent.shade400],
+    [17, "Blue Accent", Colors.blueAccent.shade400],
+    [18, "Cyan", Colors.cyan.shade600],
+    [19, "Cyan Accent", Colors.cyanAccent.shade400],
+
+    // Purples and Indigo
+    [20, "Purple", Colors.purple.shade600],
+    [21, "Deep Purple", Colors.deepPurple.shade600],
+    [22, "Purple Accent", Colors.purpleAccent.shade400],
+    [23, "Indigo", Colors.indigo.shade600],
+    [24, "Indigo Accent", Colors.indigoAccent.shade400],
+
+    // Neutral and Others
+    [25, "Black", Colors.black],
+    [26, "Brown", Colors.brown.shade600],
+    [27, "Grey", Colors.grey.shade600],
+    [28, "Blue Grey", Colors.blueGrey.shade600],
+    [29, "White", Colors.white]
   ];
 
   final colorPickerList = [
@@ -187,27 +198,6 @@ class _CanvasViewState extends State<CanvasView> {
     ]
   ];
 
-  void buildColorPicker() {
-    List temp = [];
-    List row = [];
-    for (var i = 0; i < 30; i++) {
-      if (i % 5 == 0 && i > 0) {
-        temp.add(row);
-        row = [];
-      }
-      row.add(
-          '[IconButton(onPressed: () {}, icon: const Icon(Icons.circle), color: colorsList[$i][2]), $i]');
-    }
-
-    temp.add(row);
-
-    for (var i = 0; i < colorPickerList.length; i++) {
-      print(temp[i].toString());
-    }
-    print("ColorPicker Created");
-  }
-
-  //final DatabaseHelper _dbHelper = DatabaseHelper();
 
   void setDrawingBoardStyles() {
     _controller.setStyle(
@@ -225,6 +215,7 @@ class _CanvasViewState extends State<CanvasView> {
         builder: (BuildContext) {
           return AlertDialog(
               titlePadding: const EdgeInsets.fromLTRB(16, 12, 16, 0),
+              backgroundColor: Colors.black54,
               title: Container(
                   decoration: BoxDecoration(
                       color: Colors.blue.shade700,
@@ -247,26 +238,31 @@ class _CanvasViewState extends State<CanvasView> {
                     children: [
 
                       IconButton(
+                          tooltip: colorsList[0][1],
                           onPressed: () => {Navigator.pop(context, 0)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[0][2]),
                       IconButton(
+                          tooltip: colorsList[1][1],
                           onPressed: () => {Navigator.pop(context, 1)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[1][2]),
                       IconButton(
+                          tooltip: colorsList[2][1],
                           onPressed: () => {Navigator.pop(context, 2)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[2][2]),
                       IconButton(
+                          tooltip: colorsList[3][1],
                           onPressed: () => {Navigator.pop(context, 3)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[3][2]),
                       IconButton(
+                          tooltip: colorsList[4][1],
                           onPressed: () => {Navigator.pop(context, 4)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
@@ -276,26 +272,31 @@ class _CanvasViewState extends State<CanvasView> {
                   Row(
                     children: [
                       IconButton(
+                          tooltip: colorsList[5][1],
                           onPressed: () => {Navigator.pop(context, 5)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[5][2]),
                       IconButton(
+                          tooltip: colorsList[6][1],
                           onPressed: () => {Navigator.pop(context, 6)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[6][2]),
                       IconButton(
+                          tooltip: colorsList[7][1],
                           onPressed: () => {Navigator.pop(context, 7)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[7][2]),
                       IconButton(
+                          tooltip: colorsList[8][1],
                           onPressed: () => {Navigator.pop(context, 8)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[8][2]),
                       IconButton(
+                          tooltip: colorsList[9][1],
                           onPressed: () => {Navigator.pop(context, 9)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
@@ -305,26 +306,31 @@ class _CanvasViewState extends State<CanvasView> {
                   Row(
                     children: [
                       IconButton(
+                          tooltip: colorsList[10][1],
                           onPressed: () => {Navigator.pop(context, 10)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[10][2]),
                       IconButton(
+                          tooltip: colorsList[11][1],
                           onPressed: () => {Navigator.pop(context, 11)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[11][2]),
                       IconButton(
+                          tooltip: colorsList[12][1],
                           onPressed: () => {Navigator.pop(context, 12)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[12][2]),
                       IconButton(
+                          tooltip: colorsList[13][1],
                           onPressed: () => {Navigator.pop(context, 13)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[13][2]),
                       IconButton(
+                          tooltip: colorsList[14][1],
                           onPressed: () => {Navigator.pop(context, 14)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
@@ -334,26 +340,31 @@ class _CanvasViewState extends State<CanvasView> {
                   Row(
                     children: [
                       IconButton(
+                          tooltip: colorsList[15][1],
                           onPressed: () => {Navigator.pop(context, 15)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[15][2]),
                       IconButton(
+                          tooltip: colorsList[16][1],
                           onPressed: () => {Navigator.pop(context, 16)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[16][2]),
                       IconButton(
+                          tooltip: colorsList[17][1],
                           onPressed: () => {Navigator.pop(context, 17)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[17][2]),
                       IconButton(
+                          tooltip: colorsList[18][1],
                           onPressed: () => {Navigator.pop(context, 18)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[18][2]),
                       IconButton(
+                          tooltip: colorsList[19][1],
                           onPressed: () => {Navigator.pop(context, 19)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
@@ -363,26 +374,31 @@ class _CanvasViewState extends State<CanvasView> {
                   Row(
                     children: [
                       IconButton(
+                          tooltip: colorsList[20][1],
                           onPressed: () => {Navigator.pop(context, 20)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[20][2]),
                       IconButton(
+                          tooltip: colorsList[21][1],
                           onPressed: () => {Navigator.pop(context, 21)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[21][2]),
                       IconButton(
+                          tooltip: colorsList[22][1],
                           onPressed: () => {Navigator.pop(context, 22)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[22][2]),
                       IconButton(
+                          tooltip: colorsList[23][1],
                           onPressed: () => {Navigator.pop(context, 23)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[23][2]),
                       IconButton(
+                          tooltip: colorsList[24][1],
                           onPressed: () => {Navigator.pop(context, 24)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
@@ -392,26 +408,31 @@ class _CanvasViewState extends State<CanvasView> {
                   Row(
                     children: [
                       IconButton(
+                          tooltip: colorsList[25][1],
                           onPressed: () => {Navigator.pop(context, 25)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[25][2]),
                       IconButton(
+                          tooltip: colorsList[26][1],
                           onPressed: () => {Navigator.pop(context, 26)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[26][2]),
                       IconButton(
+                          tooltip: colorsList[27][1],
                           onPressed: () => {Navigator.pop(context, 27)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[27][2]),
                       IconButton(
+                          tooltip: colorsList[28][1],
                           onPressed: () => {Navigator.pop(context, 28)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
                           color: colorsList[28][2]),
                       IconButton(
+                          tooltip: colorsList[29][1],
                           onPressed: () => {Navigator.pop(context, 29)},
                           iconSize: 42,
                           icon: const Icon(Icons.circle),
@@ -519,7 +540,6 @@ class _CanvasViewState extends State<CanvasView> {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
       setDrawingBoardStyles();
-      buildColorPicker();
     });
   }
 
