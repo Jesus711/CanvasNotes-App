8db47d9dd8f6031d902114307bd3e6cb39aaf5a8
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2024-10-15 15:52:47 -0700

    Modified Drawing Model to store the drawing's Json String instead of Blob Uint8List

--- lib/models/drawing.dart
+++ lib/models/drawing.dart
@@ -4,8 +4,11 @@ import 'dart:typed_data';
 class Drawing {
   final int ID; // ID can be null when inserting a new drawing
   final String drawingName;
-  final Uint8List drawingData;
+  final String drawingJSON;
 
-  Drawing({required this.ID, required this.drawingName, required this.drawingData});
+  Drawing({
+    required this.ID,
+    required this.drawingName,
+    required this.drawingJSON});
 
 }
