d32e0a894bb15a48ca12e57757ea1ed4c034d327
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2024-10-18 16:28:42 -0700

    Modified Canvas Size dialog content to use a SingleChildScrollView to fixed out of bounds issue when user changes the orientation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,7 +4,6 @@ import 'package:canvas_notes_flutter/pages/canvas_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 
-
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -21,74 +20,75 @@ class _HomePageState extends State<HomePage> {
   }
 
   void _createNewCanvas() async {
-
     showDialog(
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
             title: const Text("Choose Canvas Size", textAlign: TextAlign.center, style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),),
             backgroundColor: Colors.white.withOpacity(0.8),
-            content: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
+            content: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+                          backgroundColor: Colors.blue.shade800,
+                          elevation: 8,
+                        ),
+                        onPressed: () => {Navigator.pop(context, 500)},
+                        child: const Text("500x500", style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.w500),)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
                       style: ElevatedButton.styleFrom(
                         padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
                         backgroundColor: Colors.blue.shade800,
                         elevation: 8,
                       ),
-                      onPressed: () => {Navigator.pop(context, 500)},
-                      child: const Text("500x500", style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.w500),)),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
-                      backgroundColor: Colors.blue.shade800,
-                      elevation: 8,
-                    ),
-                      onPressed: () => {Navigator.pop(context, 1000)},
-                      child: const Text("1000x1000", style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.w500),)),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
-                      backgroundColor: Colors.blue.shade800,
-                      elevation: 8,
-                    ),
-                      onPressed: () => {Navigator.pop(context, 1500)},
-                      child: const Text("1500x1500", style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.w500),)),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
-                      backgroundColor: Colors.blue.shade800,
-                      elevation: 8,
-                    ),
-                      onPressed: () => {Navigator.pop(context, 2000)},
-                      child: const Text("2000x2000", style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.w500),)),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
-                      backgroundColor: Colors.blue.shade800,
-                      elevation: 8,
-                    ),
-                      onPressed: () => {Navigator.pop(context, 2500)},
-                      child: const Text("2500x2500", style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.w500),)),
-                )
-              ],
+                        onPressed: () => {Navigator.pop(context, 1000)},
+                        child: const Text("1000x1000", style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.w500),)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+                        backgroundColor: Colors.blue.shade800,
+                        elevation: 8,
+                      ),
+                        onPressed: () => {Navigator.pop(context, 1500)},
+                        child: const Text("1500x1500", style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.w500),)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+                        backgroundColor: Colors.blue.shade800,
+                        elevation: 8,
+                      ),
+                        onPressed: () => {Navigator.pop(context, 2000)},
+                        child: const Text("2000x2000", style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.w500),)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+                        backgroundColor: Colors.blue.shade800,
+                        elevation: 8,
+                      ),
+                        onPressed: () => {Navigator.pop(context, 2500)},
+                        child: const Text("2500x2500", style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.w500),)),
+                  )
+                ],
+              ),
             ),
           );
         }
@@ -99,14 +99,10 @@ class _HomePageState extends State<HomePage> {
           context,
           MaterialPageRoute(
               builder: (context) => CanvasView(
-                canvasSize: selectedSize,
-              )));
+                    canvasSize: selectedSize,
+                  )));
       setState(() {});
-
     });
-
-
-
   }
 
   void _openSavedCanvas(Drawing loadDrawing) async {
@@ -114,17 +110,14 @@ class _HomePageState extends State<HomePage> {
         context,
         MaterialPageRoute(
             builder: (context) => CanvasView(
-                  importedDrawing: loadDrawing,
-                  canvasSize: loadDrawing.canvasSize
-                )));
+                importedDrawing: loadDrawing,
+                canvasSize: loadDrawing.canvasSize)));
     setState(() {});
   }
 
   void _deleteCanvas(drawingID) async {
     _drawingDb.deleteDrawing(drawingID);
-    setState(() {
-
-    });
+    setState(() {});
   }
 
   Widget _drawingList() {
@@ -136,71 +129,82 @@ class _HomePageState extends State<HomePage> {
         }
         return Column(
           children: [
-            const Text("Tap to Open Canvas", style: TextStyle(color: Colors.white, fontSize: 20)),
+            const Text("Tap to Open Canvas",
+                style: TextStyle(color: Colors.white, fontSize: 20)),
             Expanded(
               child: ListView.builder(
-                itemCount: snapshot.data?.length ?? 0,
-                itemBuilder: (context, index) {
-                  Drawing drawing = snapshot.data![index];
-                  return Padding(
-                      padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
-                      child: Slidable(
-                          endActionPane:
-                              ActionPane(
-                                  motion: const StretchMotion(),
-                                  extentRatio: 0.3,
-                                  children: [
-                                    SlidableAction(
-                                      onPressed: (value) => {_deleteCanvas(drawing.ID)},
-                                      icon: Icons.delete,
-                                      borderRadius: BorderRadius.circular(15),
-                                      backgroundColor: Colors.red,
-                                    )
-                          ]),
-                          child: ElevatedButton(
-                            onPressed: () => {_openSavedCanvas(drawing)},
-                              style: ElevatedButton.styleFrom(
-                                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                                  backgroundColor: Colors.blue.shade600,
-                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16))
-                              ),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  itemCount: snapshot.data?.length ?? 0,
+                  itemBuilder: (context, index) {
+                    Drawing drawing = snapshot.data![index];
+                    return Padding(
+                        padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+                        child: Slidable(
+                            endActionPane: ActionPane(
+                                motion: const StretchMotion(),
+                                extentRatio: 0.3,
                                 children: [
-                                  Column(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        drawing.drawingName == "Untitled" ?
-                                        "${drawing.drawingName}${drawing.ID}" :
-                                        drawing.drawingName
-                                        ,
-                                        style: const TextStyle(
-                                            fontSize: 24,
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.w600),
-                                      ),
-                                      Text(
-                                        "Size: ${drawing.canvasSize}x${drawing.canvasSize}",
-                                        style: const TextStyle(
-                                            fontSize: 14,
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.w500),
-                                      ),
-                                      Text(
-                                        drawing.lastModifiedDate == "" ? "Created On: ${drawing.createdAtDate}" : "Last Modified: ${drawing.lastModifiedDate}",
-                                        style: const TextStyle(
-                                            fontSize: 14,
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.w500),
-                                      )
-                                    ],
-                                  ),
-                                  const Icon(Icons.image_sharp, color: Colors.white, size: 48,)
-                                ],
-                              ))));
-                }),
+                                  SlidableAction(
+                                    onPressed: (value) =>
+                                        {_deleteCanvas(drawing.ID)},
+                                    icon: Icons.delete,
+                                    borderRadius: BorderRadius.circular(15),
+                                    backgroundColor: Colors.red,
+                                  )
+                                ]),
+                            child: ElevatedButton(
+                                onPressed: () => {_openSavedCanvas(drawing)},
+                                style: ElevatedButton.styleFrom(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 16, vertical: 12),
+                                    backgroundColor: Colors.blue.shade600,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(16))),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          drawing.drawingName == "Untitled"
+                                              ? "${drawing.drawingName}${drawing.ID}"
+                                              : drawing.drawingName,
+                                          style: const TextStyle(
+                                              fontSize: 24,
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.w600),
+                                        ),
+                                        Text(
+                                          "Size: ${drawing.canvasSize}x${drawing.canvasSize}",
+                                          style: const TextStyle(
+                                              fontSize: 14,
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.w500),
+                                        ),
+                                        Text(
+                                          drawing.lastModifiedDate == ""
+                                              ? "Created On: ${drawing.createdAtDate}"
+                                              : "Last Modified: ${drawing.lastModifiedDate}",
+                                          style: const TextStyle(
+                                              fontSize: 14,
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.w500),
+                                        )
+                                      ],
+                                    ),
+                                    const Icon(
+                                      Icons.image_sharp,
+                                      color: Colors.white,
+                                      size: 48,
+                                    )
+                                  ],
+                                ))));
+                  }),
             )
           ],
         );
@@ -224,16 +228,15 @@ class _HomePageState extends State<HomePage> {
         padding: const EdgeInsets.symmetric(vertical: 8),
         child: _drawingList(),
       ),
-      floatingActionButton:
-          FloatingActionButton(
-            backgroundColor: Colors.white,
-            onPressed: _createNewCanvas,
-            tooltip: 'Create New Canvas',
-            child: const Icon(
-              Icons.add,
-              size: 32,
-            ),
-          ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white,
+        onPressed: _createNewCanvas,
+        tooltip: 'Create New Canvas',
+        child: const Icon(
+          Icons.add,
+          size: 32,
+        ),
+      ),
     );
   }
 }
@@ -251,7 +254,10 @@ class EmptyList extends StatelessWidget {
             ),
             child: Text(
               'Click on the + Icon to create a Canvas to start drawing',
-              style: TextStyle(color: Colors.white, fontSize: 28, fontWeight: FontWeight.w500),
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28,
+                  fontWeight: FontWeight.w500),
               textAlign: TextAlign.center,
             )));
   }
